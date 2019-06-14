2d1f83e557bc408da84562352aef8e584a10ba4a
andreluzz <andreluzz@gmail.com>
2019-06-14 15:52:56 -0300

    realtim first commit

--- main.go
+++ main.go
@@ -1,88 +1,24 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 
-	"github.com/gorilla/websocket"
+	"github.com/agile-work/srv-aux-realtime/socket"
 )
 
-// connected clients
-var clients = make(map[*websocket.Conn]bool)
-
-// broadcast channel
-var broadcast = make(chan Message)
-
-// Configure the upgrader
-var upgrader = websocket.Upgrader{
-	CheckOrigin: func(r *http.Request) bool {
-		return true
-	},
-}
-
-// Message object
-type Message struct {
-	Event    string      `json:"event"`
-	Message  interface{} `json:"message"`
-	Receiver string      `json:"receiver"`
-}
+var addr = flag.String("addr", ":8080", "realtime service port")
 
 func main() {
-	// Configure websocket route
-	http.HandleFunc("/", handleConnections)
-
-	// Start listening for incoming chat messages
-	go handleMessages()
-
-	// Start the wobsocket server on localhost port 8585 and log any errors
-	log.Println("Websocket server started on :8585")
-	err := http.ListenAndServe(":8585", nil)
+	flag.Parse()
+	hub := socket.NewHub()
+	go hub.Run()
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		socket.ServeWs(hub, w, r)
+	})
+	err := http.ListenAndServe(*addr, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
-	}
-}
-
-// handleConnections read messages and send to broadcast
-func handleConnections(w http.ResponseWriter, r *http.Request) {
-	// Upgrade initial GET request to a websocket
-	ws, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Printf("error %v", err)
-		return
-	}
-	// Make sure we close the connection when the function returns
-	defer ws.Close()
-
-	// Register our new client
-	clients[ws] = true
-
-	for {
-		var msg Message
-		// Read in a new message as JSON and map it to a Message object
-		err := ws.ReadJSON(&msg)
-		if err != nil {
-			log.Printf("error: %v", err)
-			delete(clients, ws)
-			break
-		}
-		// Send the newly received message to the broadcast channel
-		broadcast <- msg
-	}
-}
-
-// handleMessages redirect message to receiver
-func handleMessages() {
-	for {
-		// Grab the next message from the broadcast channel
-		msg := <-broadcast
-		// Send it out to every client that is currently connected
-		for client := range clients {
-			err := client.WriteJSON(msg)
-			if err != nil {
-				log.Printf("Error: %v", err)
-				client.Close()
-				delete(clients, client)
-			}
-		}
 	}
 }
